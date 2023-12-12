ee5964d765e77c12eaef80d422c327315b3228a4
Selkamies <85986718+Selkamies@users.noreply.github.com>
2023-12-12 13:07:29 +0200

    Added comments to database_sql.h. Log table now uses datetime column instead of date and time columns. INSERT_LOG_ROW now doesn't try to get current time, and relies on the column DEFAULT CURRENT_TIMESTAMP.

--- include/database_sql.h
+++ include/database_sql.h
@@ -5,7 +5,7 @@
  * @brief Holds #defines with SQL variables like table and column names and SQL statements.
 
  * @date Created  2023-12-08
- * @date Modified 2023-12-11
+ * @date Modified 2023-12-12
  * 
  * @copyright Copyright (c) 2023
  */
@@ -19,20 +19,25 @@
 
 #define SELECT_TABLE_EXISTS "SELECT name FROM sqlite_master WHERE type = 'table' AND name = ?;"
 
+
+
 ////////////////
 // USER TABLE //
 ////////////////
 
 #define TABLE_USER "user"
 #define COLUMN_ID_USER "id"
-// TODO: What is this?
-//#define COLUMN_RFID_ID_USER "RFID_ID"
 #define COLUMN_FIRST_NAME_USER "first_name"
 #define COLUMN_LAST_NAME_USER "last_name"
 #define COLUMN_PIN_USER "pin"
 
-// Index of the ID column in user table. Needed by sqlite3_column_int() in the callback function.
+// Index of the columns in the user table. 
+// Needed by sqlite3_column_int() etc. in callback functions to get the return values for SELECT statements.
+
 #define COLUMN_INDEX_ID_USER 0
+#define COLUMN_INDEX_FIRST_NAME_USER 1
+#define COLUMN_INDEX_LAST_NAME_USER 2
+#define COLUMN_INDEX_PIN_USER 3
 
 // SQLite notes:
 // Adding AUTOINCREMENT to PRIMARY KEY is not recommended unless you explicitly need it.
@@ -41,6 +46,7 @@
 // For INTEGER PRIMARY KEYs NOT NULL is also not needed.
 // Adding STRICT at the end causes the table to be more strict about data types.
 // NOTE: There CANNOT be a space after '\'.
+// ? is used by SQLite to bind values to.
 
 #define CREATE_TABLE_USER \
     "CREATE TABLE IF NOT EXISTS " TABLE_USER " (" \
@@ -49,7 +55,6 @@
             COLUMN_LAST_NAME_USER " TEXT NOT NULL, " \
             COLUMN_PIN_USER " TEXT NOT NULL UNIQUE) STRICT;"
 
-// ? is used by SQLite to bind values to.
 #define INSERT_USER_ROW \
     "INSERT INTO " TABLE_USER \
         " (" COLUMN_FIRST_NAME_USER ", " COLUMN_LAST_NAME_USER ", " COLUMN_PIN_USER ") " \
@@ -77,30 +82,23 @@
 #define TABLE_LOG "log"
 #define COLUMN_ID_LOG "id"
 #define COLUMN_USER_ID_LOG "user_id"
-#define COLUMN_DATE_LOG "date"
-#define COLUMN_TIME_LOG "time"
+#define COLUMN_DATETIME_LOG "datetime"
 
+#define COLUMN_INDEX_ID_LOG 0
+#define COLUMN_INDEX_USER_ID_LOG 1
+#define COLUMN_INDEX_DATETIME_LOG 2
+
+// TODO: Is there a difference between (datetime('now')) and CURRENT_TIMESTAMP?
 #define CREATE_TABLE_LOG \
     "CREATE TABLE IF NOT EXISTS " TABLE_LOG " (" \
         COLUMN_ID_LOG " INTEGER PRIMARY KEY, " \
         COLUMN_USER_ID_LOG " INTEGER NOT NULL, " \
-        COLUMN_DATE_LOG " TEXT DEFAULT CURRENT_DATE NOT NULL, " \
-        COLUMN_TIME_LOG " TEXT DEFAULT CURRENT_TIME NOT NULL, " \
+        COLUMN_DATETIME_LOG " TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL, " \
         "FOREIGN KEY (" COLUMN_USER_ID_LOG ") " \
             "REFERENCES " TABLE_USER " (" COLUMN_ID_USER ")) STRICT;"
 
-// CURRENT_DATE and CURRENT_TIME are SQLite clauses. Date will be TEXT in "yyyy-mm-dd" format
-// and time will be TEXT in "hh:mm:ss" format.
-#define INSERT_LOG_ROW \
-    "INSERT INTO " TABLE_LOG \
-        " (" COLUMN_USER_ID_LOG ", " COLUMN_DATE_LOG ", " COLUMN_TIME_LOG ") " \
-    "VALUES (?, CURRENT_DATE, CURRENT_TIME);"
-
-//#define COLUMN_LOG_TIME_LOG "LOG_TIME"
-// TODO: What is this?
-//#define COLUMN_STATUS_LOG "STATUS"
-
-//#define INSERT_LOG_ROW "INSERT INTO " TABLE_LOG " (" COLUMN_USER_ID_LOG ", " COLUMN_STATUS_LOG ") VALUES (?, ?);"
+// We don't need to insert datetime, it's set automatically.
+#define INSERT_LOG_ROW "INSERT INTO " TABLE_LOG " (" COLUMN_USER_ID_LOG ") VALUES (?);"
 
 
 
